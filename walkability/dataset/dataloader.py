59415c7a1638fa82f8764305dc57b5945397248d
Hannah Siegel <168674336+siegelhannah@users.noreply.github.com>
2026-05-22 16:23:29 -0700

    Change DataLoader num_workers from 4 to 1

--- walkability/dataset/dataloader.py
+++ walkability/dataset/dataloader.py
@@ -81,7 +81,7 @@ def get_data_loaders(base_path, name="all", batch_size=32):
         train_df = pd.read_csv(os.path.join(base_path, "train.csv"))
         train_loader = DataLoader(
             WalkabilityDataset(train_df, transform=TRAIN_TRANSFORM),
-            batch_size=batch_size, shuffle=True, num_workers=4
+            batch_size=batch_size, shuffle=True, num_workers=1
         )
     if name == "train":
         return train_loader
