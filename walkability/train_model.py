f7afae92247c1cf51c34e13c1f10c5fbfb6de81e
Hannah Siegel <Hannahs4490@gmail.com>
2026-06-02 14:17:57 -0700

    debugging

--- walkability/train_model.py
+++ walkability/train_model.py
@@ -5,6 +5,19 @@ from pytorch_lightning.callbacks import EarlyStopping
 from walkability.models.built_cnn import CNN
 from walkability.models.finetuned_resnet import Pretrained_Resnet
 
+from pytorch_lightning.callbacks import Callback
+
+# for simplified epoch progress logging:
+class EpochSummary(Callback):
+    def on_validation_epoch_end(self, trainer, pl_module):
+        metrics = trainer.callback_metrics
+        epoch = trainer.current_epoch
+        train_loss = metrics.get("train_loss_epoch", float("nan"))
+        val_loss   = metrics.get("val_loss",         float("nan"))
+        train_acc  = metrics.get("train_acc_epoch",  float("nan"))
+        val_acc    = metrics.get("val_acc",          float("nan"))
+        print(f"Epoch {epoch:3d} | train_loss: {train_loss:.4f} | val_loss: {val_loss:.4f} | train_acc: {train_acc:.4f} | val_acc: {val_acc:.4f}")
+
 
 def train_model(train_loader, val_loader, test_loader, 
                 num_classes=3, max_epochs=50, architecture="scratch"):
@@ -30,8 +43,8 @@ def train_model(train_loader, val_loader, test_loader,
         devices=1,
         enable_progress_bar=True,
         enable_model_summary=True,
-        log_every_n_steps=10,
-        # callbacks=[early_stopping], # no callbacks for debugging
+        log_every_n_steps=50, # less logs
+        callbacks=[EpochSummary()] #, early_stopping] # less epochs # don't include early_stopping yet
     )
 
     trainer.fit(model, train_loader, val_loader)
